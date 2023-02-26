c30c159ae82fa807cac35c0cc202c82a3e457ffd
Lance <ltnlikespie@gmail.com>
2023-02-26 03:17:33 -0500

    caption and date stored, search query parameters implemented

--- src/jsonResponse.js
+++ src/jsonResponse.js
@@ -1,12 +1,19 @@
 const query = require('querystring');
 const response = require('./response.js');
 
-const drawings = {};
-const getArt = (res) => {
-  const responseJSON = { drawings };
-  response.respond(res, 200, 'application/json', responseJSON);
+const drawings = [];
+let jsonResponse;
+const getArt = (res, params) => {
+  jsonResponse = {};
+  jsonResponse.results = drawings;
+  if (params.name) {
+    const searchedName = params.name.trim().toLowerCase().replaceAll(' ', '');
+    jsonResponse.results = drawings.filter((drawing) => drawing.name.trim().toLowerCase().replaceAll(' ', '') === searchedName);
+  }
+  response.respond(res, 200, 'application/json', jsonResponse);
 };
 const postArt = (req, res) => {
+  jsonResponse = {};
   const body = [];
   req.on('data', (chunk) => {
     body.push(chunk);
@@ -14,10 +21,22 @@ const postArt = (req, res) => {
   req.on('end', () => {
     const bodyString = Buffer.concat(body).toString();
     const bodyParams = query.parse(bodyString);
-    drawings[bodyParams.name] = {};
-    drawings[bodyParams.name].img = bodyParams.img;
-    drawings[bodyParams.name].name = bodyParams.name;
-    response.respond(res, 200, 'application/json', drawings);
+    const post = drawings.filter((drawing) => drawing.name === bodyParams.name);
+    if (post.length === 0) {
+      console.log("creating");
+      drawings.push({
+        img: bodyParams.img,
+        cap: bodyParams.cap,
+        name: bodyParams.name,
+        date: bodyParams.date,
+      });
+    } else {
+      drawings[drawings.findIndex((e) => e.name === bodyParams.name)].img = bodyParams.img;
+      drawings[drawings.findIndex((e) => e.name === bodyParams.name)].cap = bodyParams.cap;
+      drawings[drawings.findIndex((e) => e.name === bodyParams.name)].name = bodyParams.name;
+      drawings[drawings.findIndex((e) => e.name === bodyParams.name)].date = bodyParams.date;
+    }
+    response.respond(res, 200, 'application/json', jsonResponse);
   });
 };
 module.exports = {
--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 const http = require('http');
+const url = require('url');
+const query = require('querystring');
 
 const htmlResponse = require('./htmlResponse.js');
 const jsonResponse = require('./jsonResponse.js');
@@ -6,12 +8,14 @@ const jsonResponse = require('./jsonResponse.js');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 http.createServer((req, res) => {
-  switch (req.url) {
+  const parsedUrl = url.parse(req.url);
+  const params = query.parse(parsedUrl.query);
+  switch (parsedUrl.pathname) {
     case '/':
       htmlResponse.getHome(res);
       break;
     case '/art':
-      jsonResponse.getArt(res);
+      jsonResponse.getArt(res, params);
       break;
     case '/post':
       jsonResponse.postArt(req, res);
